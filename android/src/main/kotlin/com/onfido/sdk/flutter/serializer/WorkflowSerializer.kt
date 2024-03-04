cd3eb867c4759aef04053dc6c3611d62ea0a096d
CircleYuen <eu36@18m.dev>
2024-03-04 16:24:42 +0800

    feat: update android withLocale

--- android/src/main/kotlin/com/onfido/sdk/flutter/serializer/WorkflowSerializer.kt
+++ android/src/main/kotlin/com/onfido/sdk/flutter/serializer/WorkflowSerializer.kt
@@ -3,12 +3,9 @@ package com.onfido.sdk.flutter.serializer
 import com.onfido.workflow.WorkflowConfig
 import com.onfido.android.sdk.capture.EnterpriseFeatures
 import com.onfido.sdk.flutter.helpers.CustomMediaCallback
-
+import java.util.Locale
 fun Any.deserializeWorkflowConfig(): WorkflowConfig {
     if (this !is Map<*, *>) throw Exception("Invalid arguments for start method")
-
-    val locale = this["locale"] as? String
-        ?: throw Exception("Invalid arguments for locale")
 
     val sdkToken = this["sdkToken"] as? String
         ?: throw Exception("Invalid arguments for start method (SDK token)")
@@ -17,6 +14,11 @@ fun Any.deserializeWorkflowConfig(): WorkflowConfig {
         ?: throw Exception("Invalid arguments for start method (workflow run ID")
 
     val builder = WorkflowConfig.Builder(sdkToken, workflowRunId)
+
+    val locale = this["locale"] as? String
+    if (locale != null) {
+        builder.withLocale(Locale(locale))
+    }
 
     val withMediaCallback = this["shouldUseMediaCallback"] as? Boolean ?: false
     if (withMediaCallback) {
