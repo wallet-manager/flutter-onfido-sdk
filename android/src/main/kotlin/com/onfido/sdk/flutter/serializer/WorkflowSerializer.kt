3411815bd850970cf4a5f7f3a3983767460704f3
CircleYuen <eu36@18m.dev>
2026-09-23 16:53:02 +0800

    feat: support zh-CN in android

--- android/src/main/kotlin/com/onfido/sdk/flutter/serializer/WorkflowSerializer.kt
+++ android/src/main/kotlin/com/onfido/sdk/flutter/serializer/WorkflowSerializer.kt
@@ -1,23 +1,28 @@
 package com.onfido.sdk.flutter.serializer
 
-import com.onfido.workflow.WorkflowConfig
 import com.onfido.android.sdk.capture.EnterpriseFeatures
 import com.onfido.sdk.flutter.helpers.CustomMediaCallback
+import com.onfido.workflow.WorkflowConfig
 import java.util.Locale
+
 fun Any.deserializeWorkflowConfig(): WorkflowConfig {
     if (this !is Map<*, *>) throw Exception("Invalid arguments for start method")
 
-    val sdkToken = this["sdkToken"] as? String
-        ?: throw Exception("Invalid arguments for start method (SDK token)")
+    val sdkToken =
+            this["sdkToken"] as? String
+                    ?: throw Exception("Invalid arguments for start method (SDK token)")
 
-    val workflowRunId = this["workflowRunId"] as? String
-        ?: throw Exception("Invalid arguments for start method (workflow run ID")
+    val workflowRunId =
+            this["workflowRunId"] as? String
+                    ?: throw Exception("Invalid arguments for start method (workflow run ID")
 
     val builder = WorkflowConfig.Builder(sdkToken, workflowRunId)
 
     (this["locale"] as? String)?.let {
-        if(it == "zh-Hant") {
+        if (it == "zh-Hant") {
             builder.withLocale(Locale.TRADITIONAL_CHINESE)
+        } else if (it == "zh-Hans") {
+            builder.withLocale(Locale.SIMPLIFIED_CHINESE)
         } else {
             builder.withLocale(Locale(it))
         }
